c3c027b0adacbd3138a2c70d5535a60184777da7
kirichenkods <kirichenkods@ya.ru>
2023-12-19 23:10:02 +0300

    add solution

---
+++ src/Child.java
@@ -0,0 +1,25 @@
+public class Child extends Parent {
+    static {
+        System.out.println("Child:static 1");
+    }
+
+    {
+        System.out.println("Child:instance 1");
+    }
+
+    static {
+        System.out.println("Child:static 2");
+    }
+
+    public Child() {
+        System.out.println("Child:constructor");
+    }
+
+    public Child(String name) {
+        System.out.println("Child:name-constructor");
+    }
+
+    {
+        System.out.println("Child:instance 2");
+    }
+}
